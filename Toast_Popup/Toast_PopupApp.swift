8c3b0480a8e6768910ab9b4e04ceb7799cd363ea
dogebi2 <rainxlh@gmail.com>
2022-12-26 12:41:18 +0800

    Initial Commit

---
+++ Toast_Popup/Toast_PopupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Toast_PopupApp.swift
+//  Toast_Popup
+//
+//  Created by rain ing on 2022/12/26.
+//
+
+import SwiftUI
+
+@main
+struct Toast_PopupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
